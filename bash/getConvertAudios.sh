8326dbdc8ee0e34cb4ef86503ed77fb91492a7c8
Jaskaran Singh Puri <jaskaranpuri@gmail.com>
2019-11-02 23:07:19 +0530

    Added placeholder for S3 Bucket Name

--- bash/getConvertAudios.sh
+++ bash/getConvertAudios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws s3 sync s3://kaldicomputelambda/incoming ~/audios/ --include "*.wav" --include "*.mp3" --delete
+aws s3 sync s3://<BUCKET_NAME>/incoming ~/audios/ --include "*.wav" --include "*.mp3" --delete
 
 for audio in $(find /home/ec2-user/audios/ -type f)
 do
@@ -8,4 +8,4 @@ do
         /home/ec2-user/ffmpeg -i $audio -ar 8000 ~/audiosKaldi/$filename_noext.wav
         rm -rf $audio
 done
-aws s3 sync ~/audios/ s3://kaldicomputelambda/incoming --include "*.wav" --include "*.mp3" --delete
+aws s3 sync ~/audios/ s3://<BUCKET_NAME>/incoming --include "*.wav" --include "*.mp3" --delete
