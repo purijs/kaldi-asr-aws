d71f0e8bab783cb2ad53be87a2758158c765e554
Jaskaran Singh Puri <jaskaranpuri@gmail.com>
2019-11-03 16:52:14 +0530

    Update transcriptMaster.sh

--- bash/transcriptMaster.sh
+++ bash/transcriptMaster.sh
@@ -7,13 +7,12 @@ filesCount=$(ls /audios/*.wav | wc -l)
 cores=$(nproc)
 
 if [ $filesCount -eq 0 ]; then
-        exit 0
+
+        for audio in $(find /audios/*.wav -type f | head -$cores)
+        do
+
+                audio_name=$(basename $audio | cut -f 1 -d '.')
+                /models/transcriptWorker.sh $audio $audio_name &
+
+        done
 fi
-
-for audio in $(find /audios/*.wav -type f | head -$cores)
-do
-
-        audio_name=$(basename $audio | cut -f 1 -d '.')
-        /models/transcriptWorker.sh $audio $audio_name &
-
-done
